b7fd246482e14509e9a423e6be2055f9e7b921b9
George Pavlos <120208246+geo14150@users.noreply.github.com>
2024-01-05 18:17:36 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,114 @@
+
+ 
+  // Assuming you have a button with a specific class, e.g., "drum-button"
+  var buttons = document.querySelectorAll(".drum").length;
+  for (var i=0; i<buttons; i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+      
+      var buttonInnerHTML = this.innerHTML;
+      
+      makeSound(buttonInnerHTML);
+      buttonAnimation(buttonInnerHTML);
+  
+    });  
+  };
+  document.addEventListener("keypress", function (event) {
+    
+    makeSound(event.key);
+    buttonAnimation(event.key);
+  });
+
+  function makeSound(key){
+    switch (key) {
+      case "w":
+        var audio = new Audio("sound1.wav");
+        audio.play();
+      break;
+
+      case "a":
+        var audio = new Audio("sound2.wav");
+        audio.play();
+      break;
+      
+      case "s":
+        var audio = new Audio("sound3.wav");
+        audio.play();
+      break;
+      
+      case "d":
+        var audio = new Audio("sound4.wav");
+        audio.play();
+      break;
+      
+      case "j":
+        var audio = new Audio("sound5.wav");
+        audio.play();
+      break;
+      
+      case "k":
+        var audio = new Audio("sound6.wav");
+        audio.play();
+      break;
+      
+      case "l":
+        var audio = new Audio("sound7.wav");
+        audio.play();
+      break;
+      
+      default: console.log(buttonText);
+     }
+  }
+
+  function buttonAnimation(currentKey){
+    var activeButton = document.querySelector("."+ currentKey);
+    //Add animation to the button
+    activeButton.classList.add("pressed");
+    setTimeout(function()  {
+      activeButton.classList.remove("pressed");
+    }, "100");
+  }
+
+
+   //alert ("click me");  
+  // var audio= new Audio ("audio.mp3");
+
+//}
+//debugger;
+// pause and play music 
+//var isPlaying = false;
+//function togglePlay() {
+//    isPlaying ? audio.pause() : audio.play();
+//  };
+  
+//  audio.onplaying = function() {
+    isPlaying = true;
+//  };
+ // audio.onpause = function() {
+ //   isPlaying = false;
+//  };
+
+function  Hellboy(name,surname,age)  { 
+  this.name=name; this.surname=surname; this.age=age;
+  this.clean= function(){ 
+    alert("cleaning in progress...");
+  }
+} 
+var hellboy1 = new Hellboy("asfs", "dsgs", 19);
+
+hellboy1.clean();// an example of constructor function 
+
+
+function anotherAddEventListener(typeofevent,calback) {
+  var eventThatHappened = { 
+  eventType: "keypress",
+  key: "p",       
+  durationofkeypres: 2 
+ }
+}
+
+if (eventThatHappened.eventType === typeofevent){
+  callback(eventThatHappened);
+}
+anotherAddEventListener("keypress", function(event) {               
+     console.log(event);   
+      });
